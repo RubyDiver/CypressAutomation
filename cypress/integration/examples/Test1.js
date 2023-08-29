e6b4772553aeec3efd861901dd17487056d2867b
Paweł <tatarynowiczpawel2@gmail.com>
2023-08-29 22:58:06 +0200

    Add first test

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,1 +1,13 @@
 //cypress - Spec
+
+describe('My first test', function() {
+    it ('does not do much', function() {
+        expect(true).to.equal(false)
+    })
+    it ('do much', function() {
+        expect(true).to.equal(true)
+    })
+    it ( 'open url in browser', function() {
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+    })
+})
